6b0c15e381500657a665e7bb3623b6d7943487ea
= <=>
2023-05-05 16:08:15 +0200

    added ReleaseController

---
+++ src/main/kotlin/goerdes/chr/steam_release_query/ReleaseController.kt
@@ -0,0 +1,14 @@
+package goerdes.chr.steam_release_query
+
+import Release
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+
+@Controller
+class ReleaseController {
+
+    @QueryMapping
+    fun releases(): Iterable<Release> {
+
+    }
+}
