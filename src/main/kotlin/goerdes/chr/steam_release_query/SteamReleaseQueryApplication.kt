a6fd150ed7063b17a70187e0dca3008c6e6e82c3
christian <goerdes@predic8.de>
2023-05-05 14:42:35 +0200

    initial commit

---
+++ src/main/kotlin/goerdes/chr/steam_release_query/SteamReleaseQueryApplication.kt
@@ -0,0 +1,11 @@
+package goerdes.chr.steam_release_query
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SteamReleaseQueryApplication
+
+fun main(args: Array<String>) {
+	runApplication<SteamReleaseQueryApplication>(*args)
+}
---
+++ src/test/kotlin/goerdes/chr/steam_release_query/SteamReleaseQueryApplicationTests.kt
@@ -0,0 +1,13 @@
+package goerdes.chr.steam_release_query
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SteamReleaseQueryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
