782819ad1798878b9228738c624761a733d277b7
Christian Gördes <christian.goerdes@outlook.de>
2023-05-05 16:54:20 +0200

    connected the service with the controller

--- src/main/kotlin/goerdes/chr/steam_release_query/ReleaseController.kt
+++ src/main/kotlin/goerdes/chr/steam_release_query/ReleaseController.kt
@@ -1,16 +1,11 @@
 package goerdes.chr.steam_release_query
 
-import Release
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
 @Controller
-class ReleaseController {
-
-
+class ReleaseController(val service: ReleaseService) {
 
     @QueryMapping
-    fun releases(): Iterable<Release>? {
-        return null;
-    }
+    fun releases() = service.getAll()
 }
--- src/main/kotlin/goerdes/chr/steam_release_query/ReleaseService.kt
+++ src/main/kotlin/goerdes/chr/steam_release_query/ReleaseService.kt
@@ -1,7 +1,9 @@
 package goerdes.chr.steam_release_query
 
+import Release
 import org.springframework.stereotype.Service
 
 @Service
-class ReleaseService(provider: ReleaseJsonProvider) {
+class ReleaseService(val provider: ReleaseJsonProvider) {
+    fun getAll() = provider.query("$", List::class.java)
 }
