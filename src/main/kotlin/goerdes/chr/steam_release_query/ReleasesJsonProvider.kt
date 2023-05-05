e1f73a08e8e825e06bdde194c74de0c9f0abe218
= <=>
2023-05-05 16:19:13 +0200

    added ReleasesJsonProvider

---
+++ src/main/kotlin/goerdes/chr/steam_release_query/ReleasesJsonProvider.kt
@@ -0,0 +1,36 @@
+package goerdes.chr.steam_release_query
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.jayway.jsonpath.Configuration
+import com.jayway.jsonpath.JsonPath
+import com.jayway.jsonpath.Option
+import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Component
+import jakarta.annotation.PostConstruct
+import java.io.IOException
+
+@Component
+class ReleasesJsonProvider(private val objectMapper: ObjectMapper) {
+
+    private lateinit var jsonData: Any
+
+    @PostConstruct
+    private fun loadJsonData() {
+        try {
+            val jsonFile = ClassPathResource(JSON_FILE_NAME).file
+            jsonData = objectMapper.readValue(jsonFile, Any::class.java)
+        } catch (e: IOException) {
+            throw RuntimeException("Failed to read data from JSON file", e)
+        }
+    }
+
+    fun <T> query(jsonPathExpression: String, returnType: Class<T>): T {
+        val configuration = Configuration.defaultConfiguration().addOptions(Option.SUPPRESS_EXCEPTIONS)
+        return JsonPath.using(configuration).parse(jsonData).read(jsonPathExpression, returnType)
+    }
+
+    companion object {
+        private const val JSON_FILE_NAME = "releases.json"
+    }
+}
+
