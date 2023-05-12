3d7b12392f5f2906241bccd3fd4613588bb20afc
Christian Gördes <christian.goerdes@outlook.de>
2023-05-12 10:11:34 +0200

    added getByName

--- src/main/kotlin/goerdes/chr/steam_release_query/ReleaseController.kt
+++ src/main/kotlin/goerdes/chr/steam_release_query/ReleaseController.kt
@@ -1,5 +1,6 @@
 package goerdes.chr.steam_release_query
 
+import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
@@ -8,4 +9,7 @@ class ReleaseController(val service: ReleaseService) {
 
     @QueryMapping
     fun releases() = service.getAll()
+
+    @QueryMapping
+    fun releasesByName(@Argument name: String) = service.getByName(name)
 }
--- src/main/kotlin/goerdes/chr/steam_release_query/ReleaseService.kt
+++ src/main/kotlin/goerdes/chr/steam_release_query/ReleaseService.kt
@@ -7,4 +7,8 @@ class ReleaseService(val provider: ReleaseJsonProvider) {
     fun getAll(): List<Release> {
         return provider.query("$[*]", Array<Release>::class.java).toList()
     }
+
+    fun getByName(name: String): Any {
+        return provider.query("$[?(@.name =~ /.*$name.*/i)]", Array<Release>::class.java).toList()
+    }
 }
